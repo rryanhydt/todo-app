e3bcc5795f3ff06cf114af64c6627f235ad17504
Riyanhidayat-222 <rrynhdya.444@gmail.com>
2024-06-17 18:40:00 +0700

    editting several files

--- script.js
+++ script.js
@@ -1,48 +1,74 @@
+//const { default: axios } = require("axios");
 const taskInput = document.getElementById("task-input");
 const taskForm  = document.getElementById("task-form");
 const taskList  = document.getElementById("task-list");
 
+axios.get('http://localhost:3001/api/todos')
+    .then(function(res) {
+        for (el in res.data.data) {
+            li = document.createElement('li')
+            li.appendChild(document.createTextNode(res.data.data[el].name))
+            taskList.appendChild(li)
+        }
+    })
+    .catch(function(err) {
+        console.log(err)
+    })
+    .finally(function() {
+        
+    })
 
-taskForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const taskTitle = taskInput.value;
-    console.log(taskTitle);
+document.getElementById('btn-create-task').addEventListener('click', function(e) {
+    axios.post('http://localhost:3001/api/todos', {
+        task: taskInput.value,
+        done: false
+    }).then(function(res) {
+    }).catch(function(err) {
+    }).finally(function() {
+    })
+})
 
 
-    if (taskTitle == "") {
-        alert("Please Enter a New Task");
-    }
-    else{ 
-        const listItem = document.createElement("li");
-        listItem.innerHTML = taskTitle;
-        taskList.append(listItem);
-        let span = document.createElement('span');
-        span.innerHTML = `&times;`
-        listItem.appendChild(span);
-        taskInput.value = "";
-        saveListData();
-    }
-});
+   taskForm.addEventListener("submit", (e) => {
+       e.preventDefault();
+       const taskTitle = taskInput.value;
+       console.log(taskTitle);
 
-taskList.addEventListener("click", (e) => {
-    if (e.target.tagName == "LI") {
-        e.target.classList.toggle("checked");
-        saveListData();
-    }
 
-    if(e.target.tagName == 'SPAN') {
-        const li = e.target.parentElement;
-        li.remove();
-        saveListData();
-    }
-});
+       if (taskTitle == "") {
+           alert("Please Enter a New Task");
+       }
+       else{ 
+           const listItem = document.createElement("li");
+           listItem.innerHTML = taskTitle;
+           taskList.append(listItem);
+           let span = document.createElement('span');
+           span.innerHTML = `&times;`
+           listItem.appendChild(span);
+           taskInput.value = "";
+           saveListData();
+       }
+   });
 
-function showListData() {
-    taskList.innerHTML = localStorage.getItem("listItem");
-}
+   taskList.addEventListener("click", (e) => {
+       if (e.target.tagName == "LI") {
+           e.target.classList.toggle("checked");
+           saveListData();
+       }
 
-function saveListData(){
-    localStorage.setItem("listItem", taskList.innerHTML);
-}
+       if(e.target.tagName == 'SPAN') {
+           const li = e.target.parentElement;
+           li.remove();
+           saveListData();
+       }
+   });
 
-showListData();
+//   function showListData() {
+//       taskList.innerHTML = localStorage.getItem("listItem");
+//   }
+
+   function saveListData(){
+       localStorage.setItem("listItem", taskList.innerHTML);
+   }
+
+//   showListData();
